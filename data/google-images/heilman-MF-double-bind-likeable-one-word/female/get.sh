a3640f2a2dae6765883a57eb82099b81092b6c73
Candace <ccross@mit.edu>
2020-01-13 14:22:39 -0500

    removed comments

--- data/google-images/heilman-MF-double-bind-likeable-one-word/female/get.sh
+++ data/google-images/heilman-MF-double-bind-likeable-one-word/female/get.sh
@@ -1,79 +1,79 @@
-# wget -O agreeable_0.jpg https://fee.org/media/28679/agreeable.jpg?anchor=center&mode=crop&width=1920&rnd=131735362270000000
-# wget -O agreeable_1.jpg https://previews.123rf.com/images/khosrork/khosrork1711/khosrork171100177/90664516-happiness-woman-with-charming-smile-making-ok-gesture-being-agreeable-cute-smiling-female-with-blond.jpg
-# wget -O agreeable_2.jpg https://d24fkeqntp1r7r.cloudfront.net/wp-content/uploads/2019/12/23110408/Agreeable-Feminists.jpeg
-# wget -O agreeable_3.jpg https://thumbs.dreamstime.com/z/agreeable-young-lady-camera-agreeable-young-lady-camera-her-hands-looks-to-right-side-sweet-127220771.jpg
-# wget -O agreeable_4.jpg https://shawglobalnews.files.wordpress.com/2016/11/working-woman.jpg?quality=70&strip=all&w=720
-# wget -O agreeable_5.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190324205949/wombook1-425x239.jpg
-# wget -O agreeable_6.jpg https://st4.depositphotos.com/3516927/21068/i/1600/depositphotos_210681346-stock-photo-agreeable-businesswoman-with-marvelous-smile.jpg
-# wget -O agreeable_7.jpg https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/749/2019/05/21154653/5-21-Rhone.jpg
-# wget -O agreeable_8.jpg https://st4.depositphotos.com/8870660/28196/v/600/depositphotos_281960492-stock-video-portrait-pensive-woman-thinking-about.jpg
-# wget -O agreeable_9.jpg https://comps.canstockphoto.com/pleasant-girl-driving-a-car-pictures_csp30960689.jpg
+wget -O agreeable_0.jpg https://fee.org/media/28679/agreeable.jpg?anchor=center&mode=crop&width=1920&rnd=131735362270000000
+wget -O agreeable_1.jpg https://previews.123rf.com/images/khosrork/khosrork1711/khosrork171100177/90664516-happiness-woman-with-charming-smile-making-ok-gesture-being-agreeable-cute-smiling-female-with-blond.jpg
+wget -O agreeable_2.jpg https://d24fkeqntp1r7r.cloudfront.net/wp-content/uploads/2019/12/23110408/Agreeable-Feminists.jpeg
+wget -O agreeable_3.jpg https://thumbs.dreamstime.com/z/agreeable-young-lady-camera-agreeable-young-lady-camera-her-hands-looks-to-right-side-sweet-127220771.jpg
+wget -O agreeable_4.jpg https://shawglobalnews.files.wordpress.com/2016/11/working-woman.jpg?quality=70&strip=all&w=720
+wget -O agreeable_5.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190324205949/wombook1-425x239.jpg
+wget -O agreeable_6.jpg https://st4.depositphotos.com/3516927/21068/i/1600/depositphotos_210681346-stock-photo-agreeable-businesswoman-with-marvelous-smile.jpg
+wget -O agreeable_7.jpg https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/749/2019/05/21154653/5-21-Rhone.jpg
+wget -O agreeable_8.jpg https://st4.depositphotos.com/8870660/28196/v/600/depositphotos_281960492-stock-video-portrait-pensive-woman-thinking-about.jpg
+wget -O agreeable_9.jpg https://comps.canstockphoto.com/pleasant-girl-driving-a-car-pictures_csp30960689.jpg
 
-# wget -O fair_0.jpg https://c8.alamy.com/comp/D325FE/close-up-image-of-a-girl-with-fair-complexion-and-natural-make-up-D325FE.jpg
-# wget -O fair_1.jpg https://qph.fs.quoracdn.net/main-qimg-852d84cf520f0aaa47a92168fe4b9ff5.webp
-# wget -O fair_2.jpg https://www.yourdictionary.com/images/definitions/lg/10176.fair.jpg
-# wget -O fair_3.jpg https://image.shutterstock.com/image-photo/amazing-woman-portrait-beautiful-girl-260nw-398971693.jpg
-# wget -O fair_4.jpg https://image.shutterstock.com/image-photo/amazing-woman-portrait-beautiful-girl-260nw-720392929.jpg
-# wget -O fair_5.jpg https://thumbs.dreamstime.com/z/clear-face-woman-dark-hair-gray-eyes-fair-skin-smooth-65387633.jpg
-# wget -O fair_6.jpg https://static.toiimg.com/photo/msid-66512474/66512474.jpg?144412
-# wget -O fair_7.jpg https://image.shutterstock.com/image-photo/woman-straw-hat-fair-skin-260nw-1029953599.jpg
-# wget -O fair_8.jpg https://f4.bcbits.com/img/0011021374_10.jpg 
-# wget -O fair_9.jpg https://cdn.onebauer.media/one/lifestyle-images/celebrity/56b371dbffb595a66d46f873/Screen%20Shot%202016-02-04%20at%2015.44.49.png?quality=80&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg
+wget -O fair_0.jpg https://c8.alamy.com/comp/D325FE/close-up-image-of-a-girl-with-fair-complexion-and-natural-make-up-D325FE.jpg
+wget -O fair_1.jpg https://qph.fs.quoracdn.net/main-qimg-852d84cf520f0aaa47a92168fe4b9ff5.webp
+wget -O fair_2.jpg https://www.yourdictionary.com/images/definitions/lg/10176.fair.jpg
+wget -O fair_3.jpg https://image.shutterstock.com/image-photo/amazing-woman-portrait-beautiful-girl-260nw-398971693.jpg
+wget -O fair_4.jpg https://image.shutterstock.com/image-photo/amazing-woman-portrait-beautiful-girl-260nw-720392929.jpg
+wget -O fair_5.jpg https://thumbs.dreamstime.com/z/clear-face-woman-dark-hair-gray-eyes-fair-skin-smooth-65387633.jpg
+wget -O fair_6.jpg https://static.toiimg.com/photo/msid-66512474/66512474.jpg?144412
+wget -O fair_7.jpg https://image.shutterstock.com/image-photo/woman-straw-hat-fair-skin-260nw-1029953599.jpg
+wget -O fair_8.jpg https://f4.bcbits.com/img/0011021374_10.jpg 
+wget -O fair_9.jpg https://cdn.onebauer.media/one/lifestyle-images/celebrity/56b371dbffb595a66d46f873/Screen%20Shot%202016-02-04%20at%2015.44.49.png?quality=80&width=1800&ratio=16-9&resizeStyle=aspectfill&format=jpg
 
-# wget -O honest_0.jpg https://c8.alamy.com/comp/J32H42/young-woman-making-a-promise-isolated-on-gray-wall-background-J32H42.jpg
-# wget -O honest_1.jpg https://www.girldefined.com/wp-content/uploads/2017/09/5-reasons-girls-honest-sexual-struggles-girldefined.com_.jpg
-# wget -O honest_2.jpg https://cdn.everydaypower.com/wp-content/uploads/2018/08/honesty-quotes.jpg
-# wget -O honest_3.jpg https://image.shutterstock.com/image-photo/faithful-woman-closes-eyes-keeps-260nw-1268540023.jpg
-# wget -O honest_4.jpg https://www.aconsciousrethink.com/wp-content/uploads/2016/04/brutally-honest-woman-702x336.jpg
-# wget -O honest_5.jpg https://thoughtcatalog.files.wordpress.com/2015/01/kendall.jpg?w=615&h=409
-# wget -O honest_6.jpg https://t1.uc.ltmcdn.com/en/images/8/8/0/img_how_to_tell_if_a_girl_is_honest_10088_600.jpg
-# wget -O honest_7.jpg https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2019/06/gettyimages-1127215777_0.jpg
-# wget -O honest_8.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190309165420/woman4256-425x239.jpg
-# wget -O honest_9.jpg https://imgix.bustle.com/mic/nyvk6eahqvaxbrqhnrkjpiawrglippphokgknveoffbihulngyijrnwyau4xxvx1.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70
+wget -O honest_0.jpg https://c8.alamy.com/comp/J32H42/young-woman-making-a-promise-isolated-on-gray-wall-background-J32H42.jpg
+wget -O honest_1.jpg https://www.girldefined.com/wp-content/uploads/2017/09/5-reasons-girls-honest-sexual-struggles-girldefined.com_.jpg
+wget -O honest_2.jpg https://cdn.everydaypower.com/wp-content/uploads/2018/08/honesty-quotes.jpg
+wget -O honest_3.jpg https://image.shutterstock.com/image-photo/faithful-woman-closes-eyes-keeps-260nw-1268540023.jpg
+wget -O honest_4.jpg https://www.aconsciousrethink.com/wp-content/uploads/2016/04/brutally-honest-woman-702x336.jpg
+wget -O honest_5.jpg https://thoughtcatalog.files.wordpress.com/2015/01/kendall.jpg?w=615&h=409
+wget -O honest_6.jpg https://t1.uc.ltmcdn.com/en/images/8/8/0/img_how_to_tell_if_a_girl_is_honest_10088_600.jpg
+wget -O honest_7.jpg https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2019/06/gettyimages-1127215777_0.jpg
+wget -O honest_8.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190309165420/woman4256-425x239.jpg
+wget -O honest_9.jpg https://imgix.bustle.com/mic/nyvk6eahqvaxbrqhnrkjpiawrglippphokgknveoffbihulngyijrnwyau4xxvx1.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70
 
-# wget -O trustworthy_0.jpg https://image.shutterstock.com/image-photo/prestige-business-woman-close-up-260nw-542665954.jpg
-# wget -O trustworthy_1.jpg https://cdn.images.express.co.uk/img/dynamic/1/590x/ginger-694301.jpg
-# wget -O trustworthy_2.jpg https://www.rd.com/wp-content/uploads/2016/10/01-crack-ways-look-trustworthy-professional-networking-photos-88466647-monkeybusinessimages.jpg
-# wget -O trustworthy_3.jpg https://www.rd.com/wp-content/uploads/2016/10/05-wear-ways-look-trustworthy-professional-networking-photos-61456018-m-imagephotography.jpg
-# wget -O trustworthy_4.jpg https://i0.wp.com/ezeehow.com/wp-content/uploads/2019/06/signs-of-a-trustworthy-woman-relationship-advice-for-men.jpg?fit=1280%2C853&ssl=1
-# wget -O trustworthy_5.jpg https://media.eurekalert.org/multimedia_prod/pub/web/196338_web.jpg
-# wget -O trustworthy_6.jpg https://mysticalraven.com/wp-content/uploads/2018/05/best-friends-future-plans-071717-800x445.jpg
-# wget -O trustworthy_7.jpg https://media.istockphoto.com/photos/woman-with-a-trustworthy-face-picture-id497001003
-# wget -O trustworthy_8.jpg https://www.nysscpa.org/images/default-source/default-album/kal-visuals-592091-unsplash.jpg?sfvrsn=0&MaxWidth=1200&MaxHeight=630&ScaleUp=false&Quality=High&Method=ResizeFitToAreaArguments&Signature=EC2B04349F0EF6EB19C3D56DADC859ED
-# wget -O trustworthy_9.jpg https://www.studyfinds.org/wp-content/uploads/2019/03/arms-cheerful-coffee-1331971-816x520.jpg
+wget -O trustworthy_0.jpg https://image.shutterstock.com/image-photo/prestige-business-woman-close-up-260nw-542665954.jpg
+wget -O trustworthy_1.jpg https://cdn.images.express.co.uk/img/dynamic/1/590x/ginger-694301.jpg
+wget -O trustworthy_2.jpg https://www.rd.com/wp-content/uploads/2016/10/01-crack-ways-look-trustworthy-professional-networking-photos-88466647-monkeybusinessimages.jpg
+wget -O trustworthy_3.jpg https://www.rd.com/wp-content/uploads/2016/10/05-wear-ways-look-trustworthy-professional-networking-photos-61456018-m-imagephotography.jpg
+wget -O trustworthy_4.jpg https://i0.wp.com/ezeehow.com/wp-content/uploads/2019/06/signs-of-a-trustworthy-woman-relationship-advice-for-men.jpg?fit=1280%2C853&ssl=1
+wget -O trustworthy_5.jpg https://media.eurekalert.org/multimedia_prod/pub/web/196338_web.jpg
+wget -O trustworthy_6.jpg https://mysticalraven.com/wp-content/uploads/2018/05/best-friends-future-plans-071717-800x445.jpg
+wget -O trustworthy_7.jpg https://media.istockphoto.com/photos/woman-with-a-trustworthy-face-picture-id497001003
+wget -O trustworthy_8.jpg https://www.nysscpa.org/images/default-source/default-album/kal-visuals-592091-unsplash.jpg?sfvrsn=0&MaxWidth=1200&MaxHeight=630&ScaleUp=false&Quality=High&Method=ResizeFitToAreaArguments&Signature=EC2B04349F0EF6EB19C3D56DADC859ED
+wget -O trustworthy_9.jpg https://www.studyfinds.org/wp-content/uploads/2019/03/arms-cheerful-coffee-1331971-816x520.jpg
 
-# wget -O selfless_0.jpg https://thetempest.co/wp-content/uploads/2017/12/selfish-feature.png
-# wget -O selfless_1.jpg https://en.amerikanki.com/wp-content/uploads/2013/07/Signs_You_Are_a_Selfless_Woman.jpg
-# wget -O selfless_2.jpg https://thoughtcatalog.files.wordpress.com/2017/01/4xhqq9jv0tc-roksolana-zasiadko.jpg?w=1140&h=1143
-# wget -O selfless_3.jpg https://images.squarespace-cdn.com/content/v1/56be3fa845bf21297ea40639/1473713472845-6P2WIM5Q3BHCMBIUOSJT/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/image-asset.jpeg
-# wget -O selfless_4.jpg https://i.ytimg.com/vi/VA2dH6xcQto/maxresdefault.jpg
-# wget -O selfless_5.jpg https://images.squarespace-cdn.com/content/v1/56be3fa845bf21297ea40639/1479918534285-YVVODFWBM1D5DH8ORAFB/ke17ZwdGBToddI8pDm48kM-kIYN-CNfeEPV772NAVel7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UbfvxFNtq0a9_ojR001iak1J-2ELPkjRWg9ceOh8UAmsW07ycm2Trb21kYhaLJjddA/image-asset.png
-# wget -O selfless_6.jpg https://s.abcnews.com/images/Entertainment/gty-lena-er-170417_16x9_992.jpg
-# wget -O selfless_7.jpg http://dailybruin.com/images/43551_web.sp.9.18.regissue.lainey.ger.picao.jpg
-# wget -O selfless_8.jpg https://torontosnumber1datedoctor.com/blog/wp-content/uploads/2016/02/selfless-men-have-better-chance-with-women.jpg
-# wget -O selfless_9.jpg https://oddslifenetstorage.blob.core.windows.net/esportsinsider/2017/02/selfless-women-196357-640x320.jpg
-# sleep 3
+wget -O selfless_0.jpg https://thetempest.co/wp-content/uploads/2017/12/selfish-feature.png
+wget -O selfless_1.jpg https://en.amerikanki.com/wp-content/uploads/2013/07/Signs_You_Are_a_Selfless_Woman.jpg
+wget -O selfless_2.jpg https://thoughtcatalog.files.wordpress.com/2017/01/4xhqq9jv0tc-roksolana-zasiadko.jpg?w=1140&h=1143
+wget -O selfless_3.jpg https://images.squarespace-cdn.com/content/v1/56be3fa845bf21297ea40639/1473713472845-6P2WIM5Q3BHCMBIUOSJT/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/image-asset.jpeg
+wget -O selfless_4.jpg https://i.ytimg.com/vi/VA2dH6xcQto/maxresdefault.jpg
+wget -O selfless_5.jpg https://images.squarespace-cdn.com/content/v1/56be3fa845bf21297ea40639/1479918534285-YVVODFWBM1D5DH8ORAFB/ke17ZwdGBToddI8pDm48kM-kIYN-CNfeEPV772NAVel7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UbfvxFNtq0a9_ojR001iak1J-2ELPkjRWg9ceOh8UAmsW07ycm2Trb21kYhaLJjddA/image-asset.png
+wget -O selfless_6.jpg https://s.abcnews.com/images/Entertainment/gty-lena-er-170417_16x9_992.jpg
+wget -O selfless_7.jpg http://dailybruin.com/images/43551_web.sp.9.18.regissue.lainey.ger.picao.jpg
+wget -O selfless_8.jpg https://torontosnumber1datedoctor.com/blog/wp-content/uploads/2016/02/selfless-men-have-better-chance-with-women.jpg
+wget -O selfless_9.jpg https://oddslifenetstorage.blob.core.windows.net/esportsinsider/2017/02/selfless-women-196357-640x320.jpg
+sleep 3
 
-# wget -O accommodating_0.jpg https://athenaexeced.com/wp-content/uploads/2017/04/iStock-185266064-1080x675.jpg
-# wget -O accommodating_1.jpg https://ionemadamenoire.files.wordpress.com/2018/12/15447414536145.jpg?w=660&h=440
-# wget -O accommodating_2.jpg https://hiring.workopolis.com/wp-content/uploads/sites/3/2017/12/xsmall.jpg
-# wget -O accommodating_3.jpg https://thumbs.dreamstime.com/z/caregiver-accommodating-pillow-to-elderly-patient-female-caregiver-accommodating-pillow-to-elderly-female-patient-106676847.jpg
-# wget -O accommodating_4.jpg https://www.canadianbusiness.com/wp-content/uploads/2011/10/dd7dcafb4ae6a5a76811195ef247.jpg
-# wget -O accommodating_5.jpg http://assets-cdn.ekantipur.com/images/the-kathmandu-post/money-finance/PUNAM_SINGH_GOLCHA__240319_KT_3592-19052019074119.jpg
-# wget -O accommodating_6.jpg https://snworksceo.imgix.net/dpn/1811f377-b858-41e6-b6ee-fe3560951844.sized-1000x1000.jpg?w=1000
-# wget -O accommodating_7.jpg https://blog.gettinghired.com/hubfs/GettingHired_November2019/Images/56-4-keys-accommodating-low-cost-high-value-2017%20jpg.jpg
-# wget -O accommodating_8.jpg https://specials-images.forbesimg.com/imageserve/52753807/960x0.jpg?fit=scale
-# wget -O accommodating_9.jpg https://www.mdgadvertising.com/wp-content/uploads/2017/05/2141_1200x628_Facebook-Blog_Why-the-Future-of-Hospitality-Requires-Accommodating-Generation-Z-380x199.jpg
+wget -O accommodating_0.jpg https://athenaexeced.com/wp-content/uploads/2017/04/iStock-185266064-1080x675.jpg
+wget -O accommodating_1.jpg https://ionemadamenoire.files.wordpress.com/2018/12/15447414536145.jpg?w=660&h=440
+wget -O accommodating_2.jpg https://hiring.workopolis.com/wp-content/uploads/sites/3/2017/12/xsmall.jpg
+wget -O accommodating_3.jpg https://thumbs.dreamstime.com/z/caregiver-accommodating-pillow-to-elderly-patient-female-caregiver-accommodating-pillow-to-elderly-female-patient-106676847.jpg
+wget -O accommodating_4.jpg https://www.canadianbusiness.com/wp-content/uploads/2011/10/dd7dcafb4ae6a5a76811195ef247.jpg
+wget -O accommodating_5.jpg http://assets-cdn.ekantipur.com/images/the-kathmandu-post/money-finance/PUNAM_SINGH_GOLCHA__240319_KT_3592-19052019074119.jpg
+wget -O accommodating_6.jpg https://snworksceo.imgix.net/dpn/1811f377-b858-41e6-b6ee-fe3560951844.sized-1000x1000.jpg?w=1000
+wget -O accommodating_7.jpg https://blog.gettinghired.com/hubfs/GettingHired_November2019/Images/56-4-keys-accommodating-low-cost-high-value-2017%20jpg.jpg
+wget -O accommodating_8.jpg https://specials-images.forbesimg.com/imageserve/52753807/960x0.jpg?fit=scale
+wget -O accommodating_9.jpg https://www.mdgadvertising.com/wp-content/uploads/2017/05/2141_1200x628_Facebook-Blog_Why-the-Future-of-Hospitality-Requires-Accommodating-Generation-Z-380x199.jpg
 
-# wget -O likable_0.jpg https://ichef.bbci.co.uk/wwfeatures/live/624_351/images/live/p0/50/10/p05010ph.jpg
-# wget -O likable_1.jpg https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/7LSTMWAQIUI6TDYMN6DYUJRIRI.jpg&w=767
-# wget -O likable_2.jpg https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F39143932%2F960x0.jpg%3Ffit%3Dscale
-# wget -O likable_3.jpg http://images.huffingtonpost.com/2016-11-03-1478212816-1317299-11HabitsofRidiculouslyLikeablePeopleHP.jpg
-# wget -O likable_4.jpg https://static01.nyt.com/images/2019/11/22/us/politics/22ihw2/merlin_164769183_f607de85-06eb-4682-a4bf-989d6c227032-articleLarge.jpg?quality=75&auto=webp&disable=upscale
-# wget -O likable_5.jpg https://media3.s-nbcnews.com/j/newscms/2019_02/2707466/190107-hillary-clinton-se-501p_ff40564ccf67ca5339a18f11dce15e3f.fit-760w.jpg
-# wget -O likable_6.jpg https://www.thenation.com/wp-content/uploads/2018/10/Ana-Maria-Archilla-protest-ap-img.jpg
-# wget -O likable_7.jpg https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1133819272%2F960x0.jpg%3Ffit%3Dscale
-# wget -O likable_8.jpg https://s3.amazonaws.com/ellevate-app-uploads-production/blog_posts/6678/featured_image/large/successful_woman.jpg?1471029728
+wget -O likable_0.jpg https://ichef.bbci.co.uk/wwfeatures/live/624_351/images/live/p0/50/10/p05010ph.jpg
+wget -O likable_1.jpg https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/7LSTMWAQIUI6TDYMN6DYUJRIRI.jpg&w=767
+wget -O likable_2.jpg https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F39143932%2F960x0.jpg%3Ffit%3Dscale
+wget -O likable_3.jpg http://images.huffingtonpost.com/2016-11-03-1478212816-1317299-11HabitsofRidiculouslyLikeablePeopleHP.jpg
+wget -O likable_4.jpg https://static01.nyt.com/images/2019/11/22/us/politics/22ihw2/merlin_164769183_f607de85-06eb-4682-a4bf-989d6c227032-articleLarge.jpg?quality=75&auto=webp&disable=upscale
+wget -O likable_5.jpg https://media3.s-nbcnews.com/j/newscms/2019_02/2707466/190107-hillary-clinton-se-501p_ff40564ccf67ca5339a18f11dce15e3f.fit-760w.jpg
+wget -O likable_6.jpg https://www.thenation.com/wp-content/uploads/2018/10/Ana-Maria-Archilla-protest-ap-img.jpg
+wget -O likable_7.jpg https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1133819272%2F960x0.jpg%3Ffit%3Dscale
+wget -O likable_8.jpg https://s3.amazonaws.com/ellevate-app-uploads-production/blog_posts/6678/featured_image/large/successful_woman.jpg?1471029728
 wget -O likable_9.jpg https://cdn.vox-cdn.com/thumbor/_9ZR5D8VK7nbbKOVY9n-Ee8hEVo=/0x0:3600x2700/1200x800/filters:focal\(0x0:3600x2700\)/cdn.vox-cdn.com/uploads/chorus_image/image/48473439/GettyImages-502826670.0.jpg
 
 wget -O liked_0.jpg https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdeniserestauri%2Ffiles%2F2015%2F12%2FH1B8349-copy-1200x800.jpg
