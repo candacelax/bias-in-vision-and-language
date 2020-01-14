05f59bcabfdcdb093c09a79a6a5c9192f677d5f0
Candace <ccross@mit.edu>
2020-01-14 10:58:00 -0500

    initial upload

---
+++ data/google-images/weat6/targ1/get.sh
@@ -0,0 +1,99 @@
+wget -O male_0.jpg https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg
+wget -O male_1.jpg https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F190925152622-freddy-mcconnell.jpg
+wget -O male_2.jpg https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80
+wget -O male_3.jpg https://i.ytimg.com/vi/x_HL0wiK4Zc/maxresdefault.jpg
+wget -O male_4.jpg https://2w6kxc22rrr9mabqt1mglgait6-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/man-shrug-beard-1024x580.jpg
+wget -O male_5.jpg https://wwwimage-secure.cbsstatic.com/thumbnails/photos/w1920/marquee/11/77/27/6/hero_landscape_91e1b039-11ef-4d97-a01e-639b6ee4990a.jpg
+wget -O male_6.jpg https://media.glamour.com/photos/58336fd44e6d66172e161a3a/master/w_1600%2Cc_limit/0117-GL-LIFE01-02.jpg
+wget -O male_7.jpg https://assets.vogue.com/photos/5a0b9d136ed2ed3c2e007e85/master/pass/pine-post-tout.jpg
+wget -O male_8.jpg https://www.mantruckandbus.com/fileadmin/media/bilder/02_19/2-19-WirGlaubenAnPlatooning-startseite_1000x1500.jpg
+wget -O male_9.jpg https://quincy-network.s3.ca-central-1.amazonaws.com/wp-content/uploads/sites/8/2020/01/mug-shot-860x484.jpg
+wget -O male_10.jpg https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?cs=srgb&dl=man-wearing-black-zip-up-jacket-near-beach-smiling-at-the-736716.jpg&fm=jpg
+wget -O male_11.jpg https://images.askmen.com/1080x540/2017/01/04-123847-what_stoicism_teaches_us_about_how_to_be_a_man.jpg
+wget -O male_12.jpg https://images.unsplash.com/photo-1536548665027-b96d34a005ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80
+wget -O male_13.jpg https://pmcvariety.files.wordpress.com/2019/04/nawazuddin-siddiqui-rexfeatures_9670910s.jpg?w=1000&h=563&crop=1
+wget -O male_14.jpg https://www.mantruckandbus.com/fileadmin/media/bilder/02_19/219_05_busbusiness_interviewHeader_1485x1254.jpg
+wget -O male_15.jpg https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/best-multivitamins-for-men-1296x728-feature.jpg?w=1155&h=1528
+wget -O male_16.jpg https://image.shutterstock.com/image-photo/portrait-beautiful-smiling-man-showing-260nw-715845643.jpg
+wget -O male_17.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190324205229/40-something-man-2-1.jpg
+wget -O male_18.jpg https://goodmenproject.com/wp-content/uploads/2019/07/jared-yates-sexton-the-man-they-wanted-me-to-be.png
+wget -O male_19.jpg https://image.shutterstock.com/image-photo/nice-man-wearing-casual-clothes-260nw-1430296685.jpg
+wget -O male_20.jpg https://bdn-data.s3.amazonaws.com/uploads/2019/12/32386801_H25833993.JPG
+wget -O male_21.jpg https://www.reviewjournal.com/wp-content/uploads/2020/01/13223892_web1_cleveland_haddicks_web.jpg
+wget -O male_22.jpg https://ca-times.brightspotcdn.com/dims4/default/edcf449/2147483647/strip/true/crop/1424x1781+0+0/resize/840x1051!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F8d%2Fd0%2F3a7bdb564666864df82892e9c362%2Fsuspecta.JPG
+wget -O male_23.jpg https://cdn.cnn.com/cnnnext/dam/assets/191024211106-first-black-patient-face-transplant-exlarge-169.jpg
+wget -O male_24.jpg https://wjla.com/resources/media/286502ed-dfb4-4e2e-bf3c-9e684c81acf0-large16x9_Suspect.png?1569459112303
+wget -O male_25.jpg https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+wget -O male_26.jpg https://cdn.thedailymash.co.uk/wp-content/uploads/20190324205212/middle-aged-man-fat-2.jpg
+wget -O male_27.jpg https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/11/931/524/Jeremy-Miguel-Anderson-Clayton-County-Sheriffs-Office.jpg?ve=1&tl=1
+wget -O male_28.jpg https://www.sciencealert.com/images/2018-06/processed/joanna-nix-625948-unsplash_600.jpg
+wget -O male_29.jpg https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2019/06/the-strongest-man-in-history-brian-shaw.jpg?w=900
+wget -O male_30.jpg https://img.freepik.com/free-psd/modern-man-smiling_1194-11653.jpg?size=338&ext=jpg
+wget -O male_31.jpg https://images.unsplash.com/photo-1528900403525-dc523d4f18d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80
+wget -O male_32.jpg https://manofmany.com/wp-content/uploads/2019/06/50-Short-Haircuts-Hairstyle-Tips-for-Men.jpg
+wget -O male_33.jpg https://media3.s-nbcnews.com/j/newscms/2020_01/3168211/200102-mark-latunski-cs-1002a_1448255da44c5302c2736db12c663d9b.fit-760w.jpg
+wget -O male_34.jpg https://quincy-network.s3.ca-central-1.amazonaws.com/wp-content/uploads/sites/8/2020/01/stephen-riggins.jpg
+wget -O male_35.jpg http://nextluxury.com/wp-content/uploads/top-qualities-of-a-good-man.jpg
+wget -O male_36.jpg https://theathletic-promos.s3-us-west-2.amazonaws.com/video/Slater_Still.jpg
+wget -O male_37.jpg https://tribktla.files.wordpress.com/2019/02/santaana1.jpg?quality=85&strip=all&resize=1200,1362
+wget -O male_38.jpg https://fremontslo.com/wp-content/uploads/2019/12/Tallest-Man-Fremont-slo.jpg
+wget -O male_39.jpg https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2019/09/Royle-King-77415967_ai0a9808a-1080x675.jpg
+wget -O male_40.jpg https://static01.nyt.com/images/2019/10/13/arts/13rudd1/13rudd1-articleLarge.jpg?quality=75&auto=webp&disable=upscale
+wget -O male_41.jpg https://media.npr.org/assets/img/2019/02/15/ap_19045791021022_wide-0dbedfe0bb90477c5ab1f319ebad99843e2da989.jpg?s=1400
+wget -O male_42.jpg https://news.artnet.com/app/news-upload/2015/10/mapplethorpe-featured.jpg
+wget -O male_43.jpg https://api.time.com/wp-content/uploads/2016/10/hbo-westworld-09.jpg
+wget -O male_44.jpg https://www.justmytype.ca/wp-content/uploads/2015/12/David-Gandy-European-American-Men-1050x700.jpg
+wget -O male_45.jpg https://pmcvariety.files.wordpress.com/2019/04/travis-knight-six-billion-dollar-man.jpg?w=1000
+wget -O male_46.jpg https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80
+wget -O male_47.jpg https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555447947/shape/mentalfloss/istock_000044434608_small.jpg
+wget -O male_48.jpg https://www.themonitor.com/wp-content/uploads/sites/60/2019/08/Blake_Seth_mugshot.jpg
+wget -O male_49.jpg https://mwoy.org/sites/default/files/styles/305x420/public/participant/FY20-RobHopeHeadshot.jpeg?itok=TNO80d0N
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
