bf1029e4ace12bc915b727c690c8583c573a7c77
Candace <ccross@mit.edu>
2020-01-18 13:05:47 -0500

    updating for vilbert

--- scripts/loader.py
+++ scripts/loader.py
@@ -3,12 +3,13 @@ import re,yaml,sys
 # VilBERT
 sys.path.append('vilbert_beta')
 from vilbert.vilbert import VILBertForVLTasks as VILBERT
+from vilbert.datasets.bias_dataset import BiasLoader as BiasLoaderViLBERT
+from pytorch_pretrained_bert.tokenization import BertTokenizer
 
 # VisualBERT
 from visualbert.dataloaders.vcr import VCRLoader
 from visualbert.models.model_wrapper import ModelWrapper
 from visualbert.dataloaders.bias_dataset import BiasDataset as BiasDatasetVisualBERT
-from vilbert.datasets.bias_dataset import BiasLoader
 
 # one of these imports updates registrable in params.py in allennlp
 from allennlp.models import Model
@@ -20,76 +21,78 @@ from copy import deepcopy
 def load_data(model_params, fp=None): # TODO rename args
     # general parameters
     data = load_json(fp)
-    params_targ1 = deepcopy(model_params)
-    params_targ2 = deepcopy(model_params)
-    params_attr1_male_images = deepcopy(model_params)
-    params_attr1_female_images = deepcopy(model_params)
-    params_attr2_male_images = deepcopy(model_params)
-    params_attr2_female_images = deepcopy(model_params)
+    params_targ_X = deepcopy(model_params)
+    params_targ_Y = deepcopy(model_params)
+    params_attr_A_X = deepcopy(model_params)
+    params_attr_A_Y = deepcopy(model_params)
+    params_attr_B_X = deepcopy(model_params)
+    params_attr_B_Y = deepcopy(model_params)
     
-    params_targ1.update({'data_type' : 'targ',
-                         'image_prefix' : data['targ1']['image_prefix'],
-                         'category' : data['targ1']['category'],
-                         'examples' : data['targ1']['examples']})
-    params_targ2.update({'data_type' : 'targ',
-                         'image_prefix' : data['targ2']['image_prefix'],
-                         'category' : data['targ2']['category'],
-                         'examples' : data['targ2']['examples']})
-    
-    params_attr1_male_images.update({'data_type' : 'attr',
-                                     'image_prefix' : data['attr1']['image_prefix'],
-                                     'category' : data['attr1']['category'],
-                                     'label' : 'male_images',
-                                     'examples' : data['attr1']['examples']['male_images']})
-    params_attr1_female_images.update({'data_type' : 'attr',
-                                       'image_prefix' : data['attr1']['image_prefix'],
-                                       'category' : data['attr1']['category'],
-                                       'label' : 'female_images',
-                                       'examples' : data['attr1']['examples']['female_images']})
-    
-    params_attr2_male_images.update({'data_type' : 'attr',
-                                     'image_prefix' : data['attr2']['image_prefix'],
-                                     'category' : data['attr2']['category'],
-                                     'label' : 'male_images',
-                                     'examples' : data['attr2']['examples']['male_images']})
-    
-    params_attr2_female_images.update({'data_type' : 'attr',
-                                       'image_prefix' : data['attr2']['image_prefix'],
-                                       'category' : data['attr2']['category'],
-                                       'label' : 'female_images',
-                                       'examples' : data['attr2']['examples']['female_images']})
+    params_targ_X.update({'data_type' : 'target',
+                          'category' : data['targ1']['category'],
+                          'captions' : data['targ1']['captions'],
+                          'images' : data['targ1']['images']})
+    params_targ_Y.update({'data_type' : 'target',
+                          'category' : data['targ2']['category'],
+                          'captions' : data['targ2']['captions'],
+                          'images' : data['targ2']['images']})
+
+    category_X, category_Y = params_targ_X['category'], params_targ_Y['category']
+    params_attr_A_X.update({'data_type' : 'attr',
+                            'category' : data['attr1']['category'],
+                            'captions' : data['attr1']['captions'],
+                            'images' : data['attr1'][f'{category_X}_Images']})
+    params_attr_A_Y.update({'data_type' : 'attr',
+                            'category' : data['attr1']['category'],
+                            'captions' : data['attr1']['captions'],
+                            'images' : data['attr1'][f'{category_Y}_Images']})
+    params_attr_B_X.update({'data_type' : 'attr',
+                            'category' : data['attr2']['category'],
+                            'captions' : data['attr2']['captions'],
+                            'images' : data['attr2'][f'{category_X}_Images']})
+    params_attr_B_Y.update({'data_type' : 'attr',
+                            'category' : data['attr2']['category'],
+                            'captions' : data['attr2']['captions'],
+                            'images' : data['attr2'][f'{category_Y}_Images']})
 
     # model-specific datasets
-    targ1 = create_dataloader(params_targ1)
-    targ2 = create_dataloader(params_targ2)
-    attr1_male_images = create_dataloader(params_attr1_male_images)
-    attr1_female_images = create_dataloader(params_attr1_female_images)
-    attr2_male_images = create_dataloader(params_attr2_male_images)
-    attr2_female_images = create_dataloader(params_attr2_female_images)
+    dataloader_targ_X = create_dataloader(params_targ_X)
+    dataloader_targ_Y = create_dataloader(params_targ_Y)
+    dataloader_A_X = create_dataloader(params_attr_A_X)
+    dataloader_A_Y = create_dataloader(params_attr_A_Y)
+    dataloader_B_X = create_dataloader(params_attr_B_X)
+    dataloader_B_Y = create_dataloader(params_attr_B_Y)
     
-    return {'targ1' : targ1,
-            'targ2' : targ2,
-            'attr1_male_images' : attr1_male_images,
-            'attr1_female_images' : attr1_female_images,
-            'attr2_male_images' : attr2_male_images,  
-            'attr2_female_images' : attr2_female_images}
+    return {'targ_X' : dataloader_targ_X,
+            'targ_Y' : dataloader_targ_Y,
+            'attr_A_X' : dataloader_A_X,
+            'attr_A_Y' : dataloader_A_Y,
+            'attr_B_X' : dataloader_B_X,
+            'attr_B_Y' : dataloader_B_Y}
 
 def create_dataloader(params):
     if params.model_type.lower() == 'visualbert':
         dataset = BiasDatasetVisualBERT(params)
-        loader_params = {'batch_size': params.train_batch_size // params.num_gpus,
+        loader_params = {'batch_size': params.batch_size // params.num_gpus,
                          'num_gpus': params.num_gpus,
                          'num_workers': params.num_workers}
         return VCRLoader.from_dataset(dataset, **loader_params)
-    elif model_params.model_type.lower() == 'vilbert':
-        dataloader = BiasLoaderTrain(
-            args.train_file,
-            tokenizer,
-            seq_len=args.max_seq_length,
-            batch_size=args.train_batch_size,
-            predict_feature=args.predict_feature,
-            num_workers=args.num_workers,
-            distributed=args.distributed)
+    
+    elif params.model_type.lower() == 'vilbert':
+        tokenizer = BertTokenizer.from_pretrained(
+            params.bert_model,
+            do_lower_case=params.do_lower_case
+        )
+        dataloader = BiasLoaderViLBERT(params['examples'],
+                                       tokenizer,
+                                       seq_len=params.max_seq_length,
+                                       batch_size=params.batch_size,
+                                       predict_feature=params.get('predict_feature', False),
+                                       num_workers=params.num_workers,
+                                       distributed=params.get('distributed', False))
+        for batch in dataloader:
+            print('batch', batch)
+            exit()
     else:
         raise Exception(f'{params.model_type} is an unsupported model type')
         
