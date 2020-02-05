a2ad77ef90bf032fe0854b08752461ffe50e6913
Candace <ccross@mit.edu>
2020-02-05 14:22:13 -0500

    removed hardcoded indices for CLS, SEP, etc.

--- scripts/encoder.py
+++ scripts/encoder.py
@@ -1,4 +1,6 @@
+import torch
 from visualbert.models.model_wrapper import ModelWrapper as VisualBertModel
+from vilbert.task_utils import EvaluatingModel as ViLBERTEval
 
 class EncoderWrapper:
     def __init__(self, model):
@@ -16,24 +18,40 @@ class EncoderWrapper:
                 for idx, out in enumerate(output['sequence_output']):
                     out = out.detach().cpu()
                     input_ids = batch['bert_input_ids'][idx].cpu().tolist()
-
-                    # FIXME remove hardcode
-                    # skip SOS, CLS, SEP, period tokens
-                    end_idx = input_ids.index(102)
-                    input_ids = input_ids[:end_idx]
-                    input_ids.remove(101)
-                    if 1012 in input_ids: # period token
-                        input_ids.remove(1012)
-                    
-                    # skip SOS, CLS, END tokens
-                    word_ids = set(input_ids).difference(\
-                                        dataloader.dataset.general_indices)
-                    contextual_idx = input_ids.index(word_ids.pop())
-                    # TODO decide how to handle names OOV
+                    word_ids = [i for i in input_ids
+                                if i not in dataloader.dataset.stopword_indices]
+                    contextual_idx = input_ids.index(word_ids[0])
                     
                     # take 0-th dim corresponding to CLS token (for words + sents)
                     cls[len(cls)] = out[0,:]
                     contextual[len(contextual)] = out[contextual_idx,:]
             return cls, contextual
         else:
-            raise Exception('Other model types not yet implemented')
+            self.model.eval()
+
+            cls = {} # either word or sentence (depending on input)
+            contextual = {} # word in context
+            for batch in dataloader:
+                batch = tuple(t.cuda(non_blocking=True) for t in batch)
+                features, spatials, image_mask, question, input_mask, \
+                    segment_ids, co_attention_mask, question_id = batch
+                batch_size = features.size(0)
+
+                with torch.no_grad():
+                    vil_prediction, vil_logit, vil_binary_prediction, vision_prediction,\
+                        vision_logit, linguisic_prediction, linguisic_logit, \
+                        sequence_out \
+                        = self.model(question, features, spatials, segment_ids, input_mask,
+                                     image_mask, co_attention_mask)
+
+                    for idx, out in enumerate(sequence_out):
+                        out = out.detach().cpu()
+                        input_ids = question[idx].cpu().tolist()
+                        word_ids = [i for i in input_ids
+                                    if i not in dataloader.dataset.stopword_indices]
+                        contextual_idx = input_ids.index(word_ids[0])
+                    
+                        # take 0-th dim corresponding to CLS token (for words + sents)
+                        cls[len(cls)] = out[0,:]
+                        contextual[len(contextual)] = out[contextual_idx,:]
+            return cls, contextual
