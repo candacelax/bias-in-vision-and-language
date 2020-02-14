0dfda596c47dda094481c5e9674665cf82b0e8bc
Candace <ccross@mit.edu>
2020-02-14 17:09:15 -0500

    removed extra comments, updated install instructions

--- README.md
+++ README.md
@@ -3,10 +3,21 @@ This is the repo for our paper "Biases in Joint Visual and Language Embeddings".
 ## Installation
 Create the conda environment.
 ```bash
+git clone git@github.com:candacelax/vilbert_beta.git # forked
+git clone git@github.com:candacelax/visualbert.git # forked
+
 git clone git@github.com:candacelax/bias-grounded-bert.git
 cd bias-grounded-bert
+ln -s PATH_TO_VISUALBERT visualbert
+ln -s PATH_TO_VILBERT vilbert_beta
+
 conda env create -f environment.yml
 python -m spacy download en
+
+# add VisualBERT and ViLBERT to Python paths (consider doing in .bashrc)
+export PYTHONPATH="$PYTHONPATH:XX/bias-grounded-bert/visualbert"
+export PYTHONPATH="$PYTHONPATH:XX/bias-grounded-bert/vilbert_beta"
+export PYTORCH_PRETRAINED_BERT_CACHE="XX/bias-grounded-bert/.pytorch_pretrained_bert"
 ```
 
 
@@ -18,62 +29,15 @@ Download the pretrained models for [ViLBERT](https://drive.google.com/drive/fold
 
 
 
-## Installation
-`
-pip install spacy==2.1.0
-python -m spacy download en
-pip install neuralcoref --no-binary neuralcoref
-
-for ViLBERT, we removed the python-prctl from requirements.txt
-
-`
-Update PYTORCH_PRETRAINED_BERT_CACHE environment variable.
-
 ## Feature Extraction
 If you want to run over custom images, you'll need to compute features.
 ### VisualBert
 To compute image features, we use the same model backbone/size as VisualBERT. Detectron model id is 137851257 ([see Model Zoo](https://github.com/facebookresearch/detectron2/blob/master/MODEL_ZOO.md)).
 
 ```bash
-./scripts/image-features/extract_image_features.sh MODEL_NAME IMAGE_DIR FEATURE_PATH
+./scripts/image-features/extract_image_features.sh -m MODEL_NAME -d IMAGE_DIR -o FEATURE_PATH
 ```
 where
 * MODEL_NAME is either 'visualbert' or 'vilbert'
 * IMAGE_DIR is directory of bias test images (e.g. data/google-images/weat6)
 * FEATURE_PATH is location to save features (e.g. visualbert/image-features/google-images/weat6_features.th)
-
-
-### ViLBERT
-
-
-For running ViLBERT:
-`
-conda create -n vilbert python=3.6
-conda activate vilbert
-cd vilbert_beta
-pip install -r requirements.txt
-`
-
-For extracting visual features:
-`conda install caffe`
-
-
-For running VisualBERT:
-`
-conda create -n visual-bias python=3.7
-conda activate visual-bias
-cd vilbert_beta
-
-conda install numpy pyyaml setuptools cmake cffi tqdm pyyaml scipy ipython mkl mkl-include cython typing h5py pandas nltk spacy numpydoc scikit-learn jpeg tensorflow
-pip install tensorflow-hub
-
-#Please check your cuda version using `nvcc --version` and make sure the cuda version matches the cudatoolkit version.
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
-
-pip install -r allennlp-requirements.txt
-pip install --no-deps allennlp==0.8.0
-python -m spacy download en_core_web_sm
-pip install attrdict
-pip install pycocotools
-pip install commentjson
-`
--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
 
-# python eval_pretrained.py -folder ../logs -config ../configs/nlvr2/coco-pre-train.json
-
-# TODO make sure nothing is being masked during eval
-# TODO make sure all dropout is zero
-# TODO check for image overlap
-# TODO text labels for enc
-# TODO pooled versus sequence_output
-# TODO check indexing of cosine sim
-# TODO check all A_X, A_Y, B_X, B_Y indexing for typos
-# TODO clean up weat3.jsonl, sent-weat3.jsonl
-# TODO check image dims in bias dataset
-# TODO check images for pleasant/unpleasant
-# TODO cleaner images for C6
-# TODO check all input_ids for contextual idx in encoder
-
 import argparse
 import os
 import re
@@ -32,8 +17,7 @@ from scripts.encoder import EncoderWrapper
 from scripts.weat import weat_images_union as weat_union
 from scripts.weat import weat_images_targ_specific as weat_specific
 from scripts.weat import weat_images_intra_targ as weat_intra
-# TODO rename
-from scripts.weat.general_vals import get_general_vals
+from scripts.weat.general_vals import get_general_vals # TODO rename
 
 
 def parse_args():
@@ -44,14 +28,12 @@ def parse_args():
 
 if __name__ == '__main__':
     #--- Load params
-    # general params
-    config_fp = parse_args().config
+    config_fp = parse_args().config # general params
     with open(config_fp) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     params = AttrDict(config)
     
-    # model-specific params
-    if params.get('model_config'):
+    if params.get('model_config'): # model-specific params
         with open(params.model_config) as f:
             model_config = AttrDict(commentjson.load(f))
             params.update(model_config)
@@ -59,7 +41,7 @@ if __name__ == '__main__':
     #--- Set up logging
     t = localtime()
     timestamp = f'{t.tm_mon}-{t.tm_mday}-{t.tm_year}_' +\
-                f'{t.tm_hour:2d}:{t.tm_min:2d}:{t.tm_sec:2d}'
+                f'{t.tm_hour:02d}:{t.tm_min:02d}:{t.tm_sec:02d}'
     log_fpath = os.path.join(params.log_dir, timestamp)
     log.getLogger().addHandler(log.FileHandler(log_fpath))
     log.info(f'Params: {params}')
@@ -75,27 +57,39 @@ if __name__ == '__main__':
     writer_exp3 = csv.writer(f_exp3, delimiter=',')
     writer_gen = csv.writer(f_gen, delimiter=',')
 
-    writer_exp1.writerow(['test_name', 'test_type', 'esize', 'pval'])
-    writer_exp2.writerow(['test_name', 'test_type', 'esize', 'pval'])
-    writer_exp3.writerow(['test_name', 'test_type', 'esize_x', 'pval_x', 'esize_y', 'pval_y'])
-    writer_gen.writerow(['test_name', 'test_type', 'X_AXonAY', 'X_BXonBY',
+    writer_exp1.writerow(['test_name', 'esize', 'pval'])
+    writer_exp2.writerow(['test_name', 'esize', 'pval'])
+    writer_exp3.writerow(['test_name', 'esize_x', 'pval_x', 'esize_y', 'pval_y'])
+    writer_gen.writerow(['test_name', 'X_AXonAY', 'X_BXonBY',
                          'Y_AXonAY', 'Y_BXonBY', 'X_AonB', 'Y_AonB',
                          'X_ABXonABY', 'Y_ABXonABY'])
 
     #--- Load model
     model = load_model(params)
     encoder = EncoderWrapper(model)
-    
+        
     #--- Load tests
     log.info('Starting test loader')
-    for test_fp, chunk_fp in zip(params.fpaths_tests, params.fpaths_chunks):            
+    for vals in zip(params.tests, params.features):
+        test_fp, chunk_fp = vals
+
         # load test
         log.info(f'Loading {test_fp} {chunk_fp}')
         params.chunk_path = chunk_fp
+        params.features_fpath = chunk_fp
         dataloaders = load_data(params, fp=test_fp)
-
+        # print('TEST', test_fp, dataloaders['targ_X'].dataset.getNumUniqueImages() + \
+        #       dataloaders['targ_Y'].dataset.getNumUniqueImages() + \
+        #       dataloaders['attr_A_X'].dataset.getNumUniqueImages() + \
+        #       dataloaders['attr_A_Y'].dataset.getNumUniqueImages() + \
+        #       dataloaders['attr_B_X'].dataset.getNumUniqueImages() + \
+        #       dataloaders['attr_B_Y'].dataset.getNumUniqueImages())
+        # print('\n\n')
+        
+        
         test_type = 'sent' if re.match('.*sent.*', test_fp) else 'word'
         test_name = re.sub('sent-|.jsonl', '', os.path.basename(test_fp))
+        test_name = re.sub('one_sentence|one_word', '', test_name)
 
         # targets w/ corresponding images
         encoded_X, contextual_X = encoder.encode(dataloaders['targ_X'])
@@ -123,26 +117,24 @@ if __name__ == '__main__':
                                    'category' : dataloaders['attr_B_Y'].dataset.category}
         }
 
-
         log.info('Running experiment 1: union across attribute A_{X} and A_{Y}')
         esize, pval = weat_union.run_test(encodings, n_samples=params.num_samples)
-        writer_exp1.writerow([test_name, test_type, esize, pval])
+        writer_exp1.writerow([f'{test_name}:{test_type}', esize, pval])
 
         log.info('\n\n')
         log.info('Running experiment 2: corresponding attributes s(X, A_{X}, B_{X}) and s(Y, A_{Y}, B_{Y})')
         esize, pval = weat_specific.run_test(encodings, n_samples=params.num_samples)
-        writer_exp2.writerow([test_name, test_type, esize, pval])
-            
+        writer_exp2.writerow([f'{test_name}:{test_type}', esize, pval])  
+        
         log.info('\n\n')
         log.info('Running experiment 3: intra-target across target-specific attributes')
         esize_x, pval_x, esize_y, pval_y = weat_intra.run_test(encodings, n_samples=params.num_samples)
-        writer_exp3.writerow([test_name, test_type, esize_x, pval_x, esize_y, pval_y])
+        writer_exp3.writerow([f'{test_name}:{test_type}', esize_x, pval_x, esize_y, pval_y])
         
-
         log.info('\n\n')
         log.info('Computing general cosine similarities between cells')
         vals = get_general_vals(encodings, n_samples=params.num_samples)
-        writer_gen.writerow([test_name, test_type, vals['X_AXonAY'], vals['X_BXonBY'],
+        writer_gen.writerow([f'{test_name}:{test_type}', vals['X_AXonAY'], vals['X_BXonBY'],
                              vals['Y_AXonAY'], vals['Y_BXonBY'], vals['X_AonB'],
                              vals['Y_AonB'], vals['X_ABXonABY'], vals['Y_ABXonABY']])
 
@@ -165,24 +157,25 @@ if __name__ == '__main__':
 
             log.info('Running experiment 1 (contextual): union across attribute A_{X} and A_{Y}')
             esize, pval = weat_union.run_test(encodings, n_samples=params.num_samples)
-            writer_exp1.writerow([test_name, test_type, esize, pval])
+            writer_exp1.writerow([f'{test_name}:{test_type}', esize, pval])
             
             log.info('Running experiment 2 (contextual): corresponding attributes s(X, A_{X}, B_{X}) and s(Y, A_{Y}, B_{Y})')
             esize, pval = weat_specific.run_test(encodings, n_samples=params.num_samples)
-            writer_exp2.writerow([test_name, test_type, esize, pval])
+            writer_exp2.writerow([f'{test_name}:{test_type}', esize, pval])
+
+            log.info('\n\n')
+            log.info('Running experiment 3 (contextual): intra-target across target-specific attributes')
+            esize_x, pval_x, esize_y, pval_y = weat_intra.run_test(encodings, n_samples=params.num_samples)
+            avg_esize = (esize_x+esize_y)/2
+            avg_pval = (pval_x+pval_y)/2
+            writer_exp3.writerow([f'{test_name}:{test_type}', esize_x, pval_x, esize_y, pval_y, avg_esize, avg_pval])
             
             log.info('\n\n')
-            log.info('Running experiment 3: intra-target across target-specific attributes')
-            esize_x, pval_x, esize_y, pval_y = weat_intra.run_test(encodings, n_samples=params.num_samples)
-            writer_exp3.writerow([test_name, test_type, esize_x, pval_x, esize_y, pval_y])
-            
-
+            log.info('Computing general cosine similarities between cells (contextual)')
             vals = get_general_vals(encodings, n_samples=params.num_samples)
-            writer_gen.writerow([test_name, test_type, vals['X_AXonAY'], vals['X_BXonBY'],
+            writer_gen.writerow([f'{test_name}:{test_type}', vals['X_AXonAY'], vals['X_BXonBY'],
                                 vals['Y_AXonAY'], vals['Y_BXonBY'], vals['X_AonB'],
                                 vals['Y_AonB'], vals['X_ABXonABY'], vals['Y_ABXonABY']])
-
-        
 
         f_exp1.flush()
         f_exp2.flush()
