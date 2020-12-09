126dd6f917920739b73d88c442544216e6915767
Candace <ccross@mit.edu>
2020-12-09 11:45:40 -0500

    removing unneeded directory

--- datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from visualbert.dataloaders.vcr import VCRLoader
-from visualbert.dataloaders.bias_dataset import BiasDataset as BiasDatasetVisualBERT
-from vilbert.datasets.bias_dataset import BiasLoader as BiasLoaderViLBERT
-
